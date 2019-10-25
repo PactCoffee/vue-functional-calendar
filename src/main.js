d8dc8ce6bced582c38ef1ee94e8be78381484933
Pedro <pedrolima.0000@gmail.com>
2019-10-25 16:17:59 +0100

    add polyfill

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import FunctionalCalendar from '../index';
 import Demo from './Demo';
+import 'babel-polyfill'
 Vue.use(FunctionalCalendar, {dayNames: ['Moa', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']});
 Vue.config.productionTip = false;
 
